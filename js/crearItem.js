7def9c3f49a389d1007d8ffc0f53cde90b42f8fc
artemFedossv <a.fedossov90@gmail.com>
2024-03-12 16:47:50 -0300

    se creo un objeto para almacenar datos

--- js/crearItem.js
+++ js/crearItem.js
@@ -1,14 +1,38 @@
 const idProductos = document.querySelectorAll(".idProducto");
 const contenedorInput = document.querySelectorAll(".producto");
+
 const btnAgregar = document.getElementById("btnAgregar");
 const btnSiguiente = document.getElementById("btnSiguiente");
+
 const contenedorOriginal = document.getElementById("contenedorInput");
 const contenedorPadre = document.getElementById("contenedorPadre");
 
 const arreglo = [...contenedorInput];
 
+function Productos(id, producto, cantidad, unidad) {
+    this.id = id;
+    this.producto = producto;
+    this.cantidad = cantidad;
+    this.unidad = unidad;
+}
+
+let aux = [];
+let listaProductos = [];
+
 btnAgregar.addEventListener('click', function() {
     
+    contenedorInput.forEach(function(elemento, indice) {
+        
+        let x = elemento.value
+        aux.push(x);
+    })
+
+    const producto = new Productos(aux[0], aux[1], aux[2], aux[3])
+    
+    listaProductos.push(producto);
+
+    console.log(listaProductos)
+
     let nuevoContenedor = Array.from(contenedorOriginal.children).map(function(contenedor) {
         return contenedor.cloneNode(true);
     })
@@ -17,7 +41,7 @@ btnAgregar.addEventListener('click', function() {
         contenedorPadre.insertBefore(contenedor, contenedorOriginal);
     })
     
-    idProductos.forEach(function(producto,indice) {
+    idProductos.forEach(function(producto) {
         producto.value = parseInt(producto.value) + 1;
     })
     
@@ -25,17 +49,15 @@ btnAgregar.addEventListener('click', function() {
         if(clave == 1 || clave == 2)
         arreglo[clave].value = "";
     }
+    
 });
 
 btnSiguiente.addEventListener('click', function() {
-
+    
     
 });
 
 
-
-
- 
 
 
 
