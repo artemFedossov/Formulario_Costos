37f7de19e9f50b0d64eb669c4c0d51d1a25a1014
artemFedossv <a.fedossov90@gmail.com>
2024-03-14 00:13:08 -0300

    se aplicaron estilos

--- js/crearItem.js
+++ js/crearItem.js
@@ -7,6 +7,7 @@ const cantidadProducto = document.querySelector("#cantidadProducto");
 const unidadProducto = document.querySelector("#unidadProducto");
 const valorProducto = document.querySelector("#valorProducto");
 const cantidadComprada = document.querySelector("#cantidadComprada");
+const contenedorFormulario = document.querySelector("#formulario")
 
 btnAgregar.addEventListener("submit", agregarProductos);
 btnGenerar.addEventListener("click", calcular);
@@ -24,27 +25,36 @@ let productos = [];
 function agregarProductos(e) {
     e.preventDefault();
     let listaProducto = document.createElement("ul");
+    listaProducto.classList.add("contenedorLista")
     let nuevoProducto = new Producto(producto.value, cantidadProducto.value, unidadProducto.value, valorProducto.value, cantidadComprada.value);
     productos.push(nuevoProducto)
     
-    listaProducto.innerHTML = `<li>${producto.value}</li>
-                               <li>${cantidadProducto.value}</li>
-                               <li>${unidadProducto.value}</li>
-                               <li>${valorProducto.value}</li>
-                               <li>${cantidadComprada.value}</li>
-                               <li>${unidadProducto.value}</li>`;
+    listaProducto.innerHTML = `<li class="lista">Producto: ${producto.value} </li>
+                               <li class="lista"> se utilizaron ${cantidadProducto.value}</li>
+                               <li class="lista"> ${unidadProducto.value}</li>
+                               <li class="lista"> se compro por $${valorProducto.value}</li>`;
     
     contenedorLista.appendChild(listaProducto);
+    btnAgregar.reset();
 }
 
 function calcular() {
-
-    let costo;
-    productos.forEach(function(producto) {
-        console.log(producto.cantidadProducto)
-        costo = (parseInt(producto.cantidadProducto) * parseInt(producto.valorProducto)) / parseInt(producto.cantidadComprada);
-    })
-    console.log(costo);
+    if(productos.length != 0){
+        let contenedorCosto = document.createElement("div");
+        let costo = 0;
+        productos.forEach(function(producto) {
+            if (producto.unidadProducto === "unidades") {
+                costo += (parseInt(producto.cantidadProducto) * parseInt(producto.valorProducto)) * parseInt(producto.cantidadComprada);
+            } else {
+                costo += (parseInt(producto.cantidadProducto) * parseInt(producto.valorProducto)) / parseInt(producto.cantidadComprada);    
+            }
+        })
+    
+        contenedorCosto.innerHTML = `<p>${costo}</p>`
+        contenedorFormulario.appendChild(contenedorCosto);
+    } else {
+        alert("Primero Ingrese un producto para poder calcular el costo");
+    }
 }
 
 
