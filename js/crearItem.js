0624c8142186dcfacad4ebb90363eec607d157bc
artemFedossv <a.fedossov90@gmail.com>
2024-03-14 22:52:21 -0300

    se aplicaron comentarios y se terminaron de aplicar estilos

--- js/crearItem.js
+++ js/crearItem.js
@@ -1,3 +1,4 @@
+// Selección de los elementos del DOM
 const btnAgregar = document.querySelector("#formulario");
 const btnGenerar = document.querySelector("#btnGenerar");
 
@@ -9,9 +10,11 @@ const valorProducto = document.querySelector("#valorProducto");
 const cantidadComprada = document.querySelector("#cantidadComprada");
 const contenedorFormulario = document.querySelector("#formulario")
 
+// Agrega eventos a los botones
 btnAgregar.addEventListener("submit", agregarProductos);
 btnGenerar.addEventListener("click", calcular);
 
+// Definición de la función constructora Producto
 function Producto(producto, cantidadProducto, unidadProducto, valorProducto, cantidadComprada) {
     this.producto = producto;
     this.cantidadProducto = cantidadProducto;
@@ -20,28 +23,45 @@ function Producto(producto, cantidadProducto, unidadProducto, valorProducto, can
     this.cantidadComprada = cantidadComprada;
 }
 
+// Array en donde se almacenan los objetos Producto
 let productos = [];
 
+// Función que agrega elementos al contenedor dinamico y ademas los almacena en un array
 function agregarProductos(e) {
     e.preventDefault();
+
+    // Crea el elemento ul y ademas le agrega una clase
     let listaProducto = document.createElement("ul");
-    listaProducto.classList.add("contenedorLista")
-    let nuevoProducto = new Producto(producto.value, cantidadProducto.value, unidadProducto.value, valorProducto.value, cantidadComprada.value);
-    productos.push(nuevoProducto)
-    
-    listaProducto.innerHTML = `<li class="lista">Producto: ${producto.value} </li>
-                               <li class="lista"> se utilizaron ${cantidadProducto.value}</li>
-                               <li class="lista"> ${unidadProducto.value}</li>
-                               <li class="lista"> se compro por $${valorProducto.value}</li>`;
-    
-    contenedorLista.appendChild(listaProducto);
-    btnAgregar.reset();
+    listaProducto.classList.add("contenedorLista");
+
+    // Verifica que la cantidad utilizada no sea mayor a la cantidad comprada
+    if(parseInt(cantidadProducto.value) <= parseInt(cantidadComprada.value)){
+        let nuevoProducto = new Producto(producto.value, cantidadProducto.value, unidadProducto.value, valorProducto.value, cantidadComprada.value);
+        productos.push(nuevoProducto);
+        
+        // Inserta los elementos en una lista, para luego mostrarlos
+        listaProducto.innerHTML = `<li class="lista">Producto: ${producto.value} </li>
+                                   <li class="lista">se utilizaron ${cantidadProducto.value}</li>
+                                   <li class="lista"> ${unidadProducto.value}</li>
+                                   <li class="lista">se compro por $${valorProducto.value}</li>`;
+        
+        // Agrega la lista al contenedor   
+        contenedorLista.appendChild(listaProducto);
+        btnAgregar.reset();
+    } else {
+        alert("Su unidad utilizada no puede ser mayor a la comprada, corrijalo");
+    }
 }
 
+// Función para calcular el costo total
 function calcular() {
+    
+    // Verifica que haya elementos agregados en la lista para calcular el costo
     if(productos.length != 0){
         let contenedorCosto = document.createElement("div");
         let costo = 0;
+        
+        // Recorre el array de productos para calcular el costo total
         productos.forEach(function(producto) {
             if (producto.unidadProducto === "unidades") {
                 costo += (parseInt(producto.cantidadProducto) * parseInt(producto.valorProducto)) * parseInt(producto.cantidadComprada);
@@ -50,7 +70,8 @@ function calcular() {
             }
         })
     
-        contenedorCosto.innerHTML = `<p>${costo}</p>`
+        // Inserta en el HTML el elemento mostrando el costo
+        contenedorCosto.innerHTML = `<p class="lista">su costo es de <span class="costo">$${costo}</span></p>`
         contenedorFormulario.appendChild(contenedorCosto);
     } else {
         alert("Primero Ingrese un producto para poder calcular el costo");
